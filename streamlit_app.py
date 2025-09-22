ac912498c6fd5c9fe41a862d195adfaa7a0e31f5
Daniel Aqil <110409976+dnlaql@users.noreply.github.com>
2025-09-22 15:57:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
+import streamlit as st
+from auth.login import login_user
+from config.supabase_config import supabase
+from datetime import datetime
+
+# ---------------- Debug User List ----------------
 st.subheader("📋 Debug User List")
-
 users = supabase.table("user_test").select("*").execute()
-
 st.write(users.data)
