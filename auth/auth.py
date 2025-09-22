399fd50e7e505d3999f41ea32cb8ba7d40509296
Daniel Aqil <110409976+dnlaql@users.noreply.github.com>
2025-09-22 15:00:22 +0800

    Update auth.py
    Easy Template

--- auth/auth.py
+++ auth/auth.py
@@ -1,1 +1,12 @@
-da
+from supabase_config import supabase
+
+def login_user(email, password):
+    """
+    Check user login based on table 'user'
+    Return True + email kalau berjaya, False kalau gagal
+    """
+    res = supabase.table("user").select("*").eq("email", email).eq("password", password).execute()
+    if res.data and len(res.data) > 0:
+        return True, email
+    else:
+        return False, None
