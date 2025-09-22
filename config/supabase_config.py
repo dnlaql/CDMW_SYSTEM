4ee1ed1e23ef4c6a3a2235aca6e78cb6b4e3d505
Daniel Aqil <110409976+dnlaql@users.noreply.github.com>
2025-09-22 15:52:24 +0800

    Update supabase_config.py

--- config/supabase_config.py
+++ config/supabase_config.py
@@ -1,6 +1,7 @@
 from supabase import create_client
 import streamlit as st
 
+# Guna secret yang dah set dalam Streamlit Cloud
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
