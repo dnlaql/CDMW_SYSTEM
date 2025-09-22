685ffa6766f19a6dc929ea37cdcd0657f3cf8777
Daniel Aqil <110409976+dnlaql@users.noreply.github.com>
2025-09-22 15:05:19 +0800

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -5,7 +5,7 @@ def login_user(email, password):
     Check user login based on table 'user'
     Return True + email kalau berjaya, False kalau gagal
     """
-    res = supabase.table("user").select("*").eq("email", email).eq("password", password).execute()
+    res = supabase.table("user").select("*").eq("username", email).eq("password", password).execute()
     if res.data and len(res.data) > 0:
         return True, email
     else:
