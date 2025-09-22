383b9c8ee4c2b3331d4d68aac3db1ef8b65c53a7
Daniel Aqil <110409976+dnlaql@users.noreply.github.com>
2025-09-22 15:23:47 +0800

    Update login.py

--- auth/login.py
+++ auth/login.py
@@ -3,10 +3,10 @@ from config.supabase_config import supabase
 def login_user(username, password):
     """
     Check user login based on table 'user'
-    Return True + email kalau berjaya, False kalau gagal
+    Return True + username kalau berjaya, False kalau gagal
     """
-    res = supabase.table("user").select("*").eq("username", email).eq("password", password).execute()
+    res = supabase.table("user").select("*").eq("username", username).eq("password", password).execute()
     if res.data and len(res.data) > 0:
-        return True, email
+        return True, username
     else:
         return False, None
