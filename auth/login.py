49afb50da20ce21cb74a3f4b9660b83126b63d64
Daniel Aqil <110409976+dnlaql@users.noreply.github.com>
2025-09-22 15:33:55 +0800

    Update login.py

--- auth/login.py
+++ auth/login.py
@@ -1,6 +1,14 @@
+from config.supabase_config import supabase
+
 def login_user(username, password):
-    res = supabase.table("user").select("*").eq("username", username).execute()
-    print("DEBUG USERNAME ONLY:", res.data)
+    """
+    Check user login from table 'user_test'
+    Return True + username kalau berjaya, False kalau gagal
+    """
+    res = supabase.table("user_test").select("*").eq("username", username).eq("password", password).execute()
+
+    # Debug kalau nak tengok apa Supabase return
+    # print("DEBUG LOGIN:", res)
 
     if res.data and len(res.data) > 0:
         return True, username
