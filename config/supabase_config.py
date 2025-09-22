62bc9d8167ab563e1f1f5ed1175e7aed1cc63640
Daniel Aqil <110409976+dnlaql@users.noreply.github.com>
2025-09-22 15:51:04 +0800

    Update supabase_config.py

--- config/supabase_config.py
+++ config/supabase_config.py
@@ -1,10 +1,7 @@
 from supabase import create_client
-import os
-from dotenv import load_dotenv
+import streamlit as st
 
-load_dotenv()
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
